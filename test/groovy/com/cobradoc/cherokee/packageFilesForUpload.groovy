606c0213b69c8f76e0fa264ade087ad0e683a18b
cdrchops <standtrooper@gmail.com>
2019-10-18 16:26:11 -0500

    refactored for version 0.14

--- test/groovy/com/cobradoc/cherokee/packageFilesForUpload.groovy
+++
@@ -1,7 +0,0 @@
-package com.cobradoc.cherokee
-
-for (int i = 1; i <= 100; i++) {
-    def f = new File("page${i}.txt").getText("UTF-8")
-    def text = f.replaceAll("<en>", "");
-    new File("page${i}_latin.txt").write(text)
-}
